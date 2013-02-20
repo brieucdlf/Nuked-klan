a871d5e6b809e1341ee332d5189bd26427cb6891
Yurty <dlf.brieuc@gmail.com>
2013-02-20 15:41:46 +0100

    Second commit
    Adding the notif patch for the version 1.7.8 & 1.7.9

---
+++ Patch notif 1.7.8/User/user.js
@@ -0,0 +1,8 @@
+///////////////////////////
+/* Patch login notif     */
+/* http://nkhelp.fr      */
+/* Made by Yurty         */
+/* fichier js de Kipcool */
+///////////////////////////
+
+document.write('<link rel="stylesheet" type="text/css" href="modules/User/user.css">');
